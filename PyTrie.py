1788c959c83603ee444397ecd5e1b52cd3bc527c
paula <paularelt@gmail.com>
2022-02-06 11:40:11 -0500

    get words methods

--- PyTrie.py
+++ PyTrie.py
@@ -18,8 +18,31 @@ class Trie(object):
 			if parent:
 				print("PARENT",parent)
 			print("KEY",k)
+			if root.map[k].is_leaf:
+				print("IS_LEAF")
 			print()
 			self.traverse(root.map[k], k)
+			
+	def get_words_help(self, node, xlist, path, plen):
+		if not node:
+			return
+
+		if node.is_leaf:
+			xlist.append(''.join(path[:plen]))
+			return
+
+		for k in node.map.keys():
+			if(len(path) <= plen):
+				path.append(k)
+			else:
+				path[plen] = k
+			self.get_words_help(node.map[k], xlist, path, plen+1)
+
+	def get_words(self):
+		xlist = []
+		for start in self.map.keys():
+			self.get_words_help(self, xlist, [], 0)
+		return xlist
 
 	def exists(self, word):
 		cur = self
@@ -41,7 +64,13 @@ t = Trie()
 
 t.insert("hello")
 t.insert("heck")
+t.insert("help")
 
+print("Words:")
+for word in t.get_words():
+	print(word)
+
+#'''
 print("exists")
 print("hello", t.exists("hello")) # True
 print("heck", t.exists("heck")) # True
@@ -53,3 +82,4 @@ print("hell", t.starts_with("hell")) # True
 print("hec", t.starts_with("hec")) # True
 print("head", t.starts_with("head")) # False
 print("hear", t.starts_with("hear")) # False
+#'''
