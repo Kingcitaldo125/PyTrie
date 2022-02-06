3c814375ccee826e19451709efd7585a3ef817e9
paula <paularelt@gmail.com>
2022-02-06 10:08:52 -0500

    Add more methods

--- PyTrie.py
+++ PyTrie.py
@@ -18,13 +18,38 @@ class Trie(object):
 			if parent:
 				print("PARENT",parent)
 			print("KEY",k)
+			print()
 			self.traverse(root.map[k], k)
+
+	def exists(self, word):
+		cur = self
+		for w in word:
+			if w not in cur.map:
+				return False
+			cur = cur.map[w]
+		return cur.is_leaf
+
+	def starts_with(self, prefix):
+		cur = self
+		for p in prefix:
+			if p not in cur.map:
+				return False
+			cur = cur.map[p]
+		return True
 
 t = Trie()
 
 t.insert("hello")
 t.insert("heck")
 
-t.traverse(t)
+print("exists")
+print("hello", t.exists("hello")) # True
+print("heck", t.exists("heck")) # True
+print("head", t.exists("head")) # False
+print("hear", t.exists("hear")) # False
 
-print(t.map)
+print("starts with")
+print("hell", t.starts_with("hell")) # True
+print("hec", t.starts_with("hec")) # True
+print("head", t.starts_with("head")) # False
+print("hear", t.starts_with("hear")) # False
